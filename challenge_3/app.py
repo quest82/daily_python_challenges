85ce3a948a80adf0f965846f981ffd474115e7f0
quest82 <awoniyioluseyi@gmail.com>
2026-08-08 23:22:38 +0100

    Added a function: add_setting which allows user to add new settings

--- challenge_3/app.py
+++ challenge_3/app.py
@@ -21,4 +21,17 @@ def view_settings(current_settings = {}):
 
 # print(view_settings(test_settings))
 
-# # 
+# # You should define a function named add_setting with two parameters representing a dictionary of settings and a tuple containing a key-value pair
+
+def add_setting (current_settings = {}, new_setting =('key', 'value')):
+    key, value = new_setting[0].lower(), new_setting[1].lower()
+
+    if key in current_settings.keys():
+        return f"Setting '{key}' already exists! Cannot add a new setting with this name."
+
+    current_settings[key] = value
+    return f"Setting '{key}' added with value '{value}' successfully!."
+
+# print(add_setting(test_settings, ('Brightness', '80%')))
+
+
