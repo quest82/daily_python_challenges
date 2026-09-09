8be2f95caf03777cccc79f8f638707266aa85142
quest82 <awoniyioluseyi@gmail.com>
2026-09-09 22:02:39 +0100

    Progress report: still stuck on separating the response

--- challenge_9/app.py
+++ challenge_9/app.py
@@ -3,4 +3,8 @@ def main():
     print(get_gc_content(data))
 
 def get_gc_content(x):
-    
+    raw_sequences = x.strip()
+    separated_sequences = [single_seq.strip() for single_seq in raw_sequences.split('>') if single_seq.strip()]
+
+
+main()
