8d6471de96e4cbc1a11eb696d92140e1997949fb
quest82 <awoniyioluseyi@gmail.com>
2026-09-11 15:49:30 +0100

    Progress report: found a solution to prompting for the data and organising the input

--- challenge_9/app.py
+++ challenge_9/app.py
@@ -1,10 +1,27 @@
 def main():
-    data = input("Enter your sequences")
-    print(get_gc_content(data))
 
-def get_gc_content(x):
-    raw_sequences = x.strip()
-    separated_sequences = [single_seq.strip() for single_seq in raw_sequences.split('>') if single_seq.strip()]
+    sequence_header = ''
+    sequence_database = {}
+
+    print('Enter your all sequences one by one below and press CTRL + D to get your result\n')
+
+    while True:
+        try:
+            line = input().strip()
+        except EOFError: 
+            break 
+        else:
+            if not line:
+                continue
+            if line.startswith('>'):
+                sequence_header = line[1:].strip()
+                sequence_database[sequence_header] = ''
+            else:
+                sequence_database[sequence_header] += line
+    result = get_gc_content(sequence_database)
+    print(result)
+def get_gc_content(dictionary):
+    return dictionary
 
 
 main()
