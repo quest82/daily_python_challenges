5049fe99a626f9a0d830b2d5283cccf03407fb25
quest82 <awoniyioluseyi@gmail.com>
2026-09-11 16:26:38 +0100

    Progress report: added logic to get non word entries

--- challenge_10/app.py
+++ challenge_10/app.py
@@ -2,6 +2,8 @@ def reverse_sentence(x):
     
     reversed_array = []
     for word in x.split(" "):
+        if not word:
+            continue
         reversed_array.insert(0, word)
 
     # sentence = " ".join(reversed_array)
