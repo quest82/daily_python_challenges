d48b9ef7c549785e005beb725fd0a348daa6de1f
quest82 <awoniyioluseyi@gmail.com>
2026-09-24 15:26:31 +0100

    Challenge 12 Report:Function can check for average of three consecutive days

--- challenge_12/app.py
+++ challenge_12/app.py
@@ -2,17 +2,39 @@ def too_much_screen_time(hours):
     if not hours:
         return "Function requires an array with at least one value"
 
+    screen_time = False
+
     for i in hours: # If any day in the array is above 10, it returns True for too much screen time
         if i > 10:
-            return True
+            screen_time = True
 
     avg_of_seven = sum(hours) / len(hours) # If the avg screen time for the week is 6 or above, it returns True
     if avg_of_seven >= 6:
-        return True 
+        screen_time = True 
 
-    # array_of_threes = []
-    # for i in hours:
-    #     if 
+
+    while True:
+        if len(hours) < 3:
+            break
+
+        array_of_threes = []
+        for i in hours:
+            if len(array_of_threes) < 3:
+                array_of_threes.append(i)
+
+        if (sum (array_of_threes) / len(array_of_threes)) < 8:
+            screen_time = False
+            del hours[0]
+
+        else:
+            screen_time = True
+            break
+    return screen_time
+
+print(too_much_screen_time([3, 3, 5, 8, 8, 9, 4]))
+
+
+            
 
 
 
