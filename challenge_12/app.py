6a5f658a9535cb4b5ae8b0ad1f92d6e1cb3e5bd7
quest82 <awoniyioluseyi@gmail.com>
2026-09-24 14:43:36 +0100

    Challenge 12 Report: Cleaning up code to start fresh. Function can check for 10 hour screen time

--- challenge_12/app.py
+++ challenge_12/app.py
@@ -1,62 +1,8 @@
 def too_much_screen_time(hours):
-    # For average of three
+    if not hours:
+        return "Function requires an array with at least one value"
 
-    start = 0
-    stop = 3
-    guage = True
+    for i in hours: # If any day in the array is above 10, it returns True for too much screen time
+        if i > 10:
+            return True
 
-    while True:
-        x, y, z = hours[start:stop]
-        average = int(round((x + y + z)/3))
-        
-        print(x, y, z, start, stop)
-        if average < 8:
-            start += 1
-            stop +=1
-            guage = False
-        else:
-            guage = True
-            break
-
-        if (stop > len(hours)) & (average < 8):
-            break
-    return guage
-    
-   
-    
-print(too_much_screen_time([1, 10, 10, 10]))
-    
-
-#     for i in hours:
-#         if i > 10:
-#             return True
-
-# def three_avg(hours):
-#     loop_counter = 1
-#     starter = 0
-#     index = starter
-#     three_total = 0
-#     example = []
-
-#     while True:
-#         three_total += hours[index]
-#         example.append(hours[index])
-#         if loop_counter == 3:
-#             avg = int(round(three_total / 3))
-#             if avg >= 8:
-#                 return True, example
-#             else:
-#                 starter += 1
-#                 loop_counter = 1
-#                 example = []
-
-
-#         loop_counter +=1
-#         index += 1
-                
-
-        
-
-
-
-    
