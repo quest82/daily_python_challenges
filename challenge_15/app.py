6e14bc55abce30d5d1f4a43d96b6f4db9ddeeb63
quest82 <awoniyioluseyi@gmail.com>
2026-09-25 13:50:40 +0100

    Challenge 15 Report: Cleaned up the code

--- challenge_15/app.py
+++ challenge_15/app.py
@@ -1,6 +1,9 @@
 def is_mirror(str1, str2):
+    def only_letters(s):
+        return ''.join(c for c in s.strip() if c.isalpha())
+    
     str2 = str2.strip()
-    array_2 = str2.split(' ')
+    array_2 = str2.split()
     array_3 =[]
     for word in array_2:
         word_array = []
@@ -11,9 +14,8 @@ def is_mirror(str1, str2):
 
         part_result = ''.join(word_array)
         array_3.insert(0, part_result)
-        
-        word_array = []
 
-    return ''.join(str1.strip().split()) == ''.join(array_3)
 
-print(is_mirror("helloworld", "helloworld"))
+    return only_letters(str1) == ''.join(array_3)
+
+print(is_mirror("abc def", "cba fed"))
