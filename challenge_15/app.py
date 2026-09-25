9c917aa7e095ae60746e1f1318fb157c49803ab3
quest82 <awoniyioluseyi@gmail.com>
2026-09-25 13:13:03 +0100

    Challenge 15 Report: Found a way to remove non alphabet characters

--- challenge_15/app.py
+++ challenge_15/app.py
@@ -1,3 +1,12 @@
 def is_mirror(str1, str2):
+    str2 = str2.strip()
+    array_2 = str2.split(' ')
+    array_3 =[]
+    for word in array_2:
+        for letter in word:
+            if letter.isalpha():
+                array_3.insert(0, letter)
 
-    return str1
+    return array_3
+
+print(is_mirror("Hello World", "!dlroW !olleH"))
